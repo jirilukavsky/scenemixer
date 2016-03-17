7bd963281ef5e23fcd63ae7a195096d4c470c845
Jiri Lukavsky <lukavsky@praha.psu.cas.cz>
2016-03-17 13:37:03 +0100

    Changed to use skimage instead of cv2. Better blur circle with cosine alpha gradient and constant colour.

--- tools.py
+++ tools.py
@@ -1,8 +1,7 @@
 from __future__ import division
 import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
+from skimage import io, transform, filters, color
+from skimage import img_as_uint, img_as_float
 
 def crop_and_resize_image(img, new_size, grayscale = False):
     height, width, channels = img.shape
@@ -14,44 +13,42 @@ def crop_and_resize_image(img, new_size, grayscale = False):
     center    = (width // 2, height // 2)
     img_cropped = img[(center[1] - csize2[1]):(center[1] + csize2[1]),
                       (center[0] - csize2[0]):(center[0] + csize2[0]), :]
-    img_resized = cv2.resize(img,(new_size[1], new_size[0]), interpolation = cv2.INTER_CUBIC)
+    img_resized = transform.resize(img_cropped,(new_size[1], new_size[0]))
     if grayscale:
-        img_resized = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
-        img_resized = cv2.cvtColor(img_resized, cv2.COLOR_GRAY2BGR) # keep all channels
+        img_resized = color.gray2rgb(color.rgb2gray(img_resized)) # keep 3 channels
     return img_resized
-
-def show_image(image):
-    plt.axis("off")
-    if len(image.shape) > 2:
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    else:
-        plt.imshow(image, cmap = cm.Greys_r)
-    plt.show()
 
 def combine_images(img_in, img_out, radius):
     assert img_in.shape == img_out.shape
+    assert img_in.dtype == img_out.dtype
     h = img_in.shape[0]
     w = img_in.shape[1]
     x = np.arange(0, w); y = np.arange(0, h)
     xx, yy = np.meshgrid(x, y)
     mask_in = np.sqrt((xx - w // 2) ** 2 + (yy - h // 2) ** 2) <= radius
-    img_new = np.zeros(img_in.shape, dtype = np.uint8)
+    img_new = np.zeros(img_in.shape, dtype = img_in.dtype)
     img_new[mask_in] = img_in[mask_in]
     img_new[np.logical_not(mask_in)] = img_out[np.logical_not(mask_in)]
     return img_new
 
-def add_ring(img, radius, width, colour = (128,128,128), blur = 0):
+def make_ring_for_image(img, radius, width50, colour = (128, 128, 128)):
     h = img.shape[0]
     w = img.shape[1]
     x = np.arange(0, w); y = np.arange(0, h)
-    imr = np.zeros((h, w, 4), dtype = np.uint8)
-    ima = np.zeros((h, w, 1), dtype = np.uint8)
-    col = (colour[0], colour[1], colour[2], 255)
-    cv2.circle(imr, (w // 2, h // 2), radius, col, width)    
-    cv2.circle(ima, (w // 2, h // 2), radius, 255, width)    
-    if blur > 0:
-        #ima = cv2.blur(ima, (blur, blur))
-        ima = cv2.GaussianBlur(ima, ksize = (0, 0), sigmaX = blur)
-        imr[:,:,:3] = 128 
-        imr[:,:,3]  = ima / (ima.max() / 255.)
-    return imr
+    xx, yy = np.meshgrid(x, y)
+    distance = np.sqrt((xx - w // 2) ** 2 + (yy - h // 2) ** 2)
+    im = - (np.abs(distance - radius) - width50) / width50
+    im[im < -1.] = -1   # now -1 .. +1
+    im = (1. - im) * np.pi / 2.
+    im = (np.cos(im) + 1.) / 2
+    im4 = np.zeros((h, w, 4), dtype = img.dtype)
+    col = np.array(colour).reshape((1, 1, 3)) / 256.
+    im4[:, :, :3] = col
+    im4[:, :, 3] = im
+    return (im, im4)
+    
+def add_ring(img, img4):
+    alpha = img4[:,:,3][..., None]
+    im = img[:,:,:3] * (1 - alpha) + img4[:,:,:3] * alpha
+    return im
+
